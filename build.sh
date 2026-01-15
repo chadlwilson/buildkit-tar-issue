8c4c78af6053d677513255eb8869d7e6f61244c7
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2026-01-15 13:34:03 +0800

    arm64 only

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ echo "Building with buildkit=$BUILDKIT_VERSION"
 docker buildx rm --force tmp-builder || true
 docker buildx create --use --name tmp-builder --driver-opt image=moby/buildkit:$BUILDKIT_VERSION
 docker buildx inspect --bootstrap tmp-builder
-docker buildx build --pull --platform linux/amd64,linux/arm64 . --tag latest
+docker buildx build --pull --platform linux/arm64 . --tag latest
